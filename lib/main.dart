67ce435ac2284f5f06ecb5cdb95a72508694b8d0
carlstep <carlstep333@me.com>
2021-05-11 22:06:28 +0200

    hot reload issue commit A

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-// main() is the staring point for all apps followed by runApp(....)
+// main() is the starting point for all apps followed by runApp(....)
 void main() {
   runApp(
     MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.red,
         title: Text(
           'I Am Rich',
         ),
