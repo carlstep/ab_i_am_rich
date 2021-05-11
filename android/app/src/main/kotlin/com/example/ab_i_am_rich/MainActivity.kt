9e340c2ff663eee5920d54e273c974a369415004
carlstep <carlstep333@me.com>
2021-05-11 21:44:56 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ab_i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ab_i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// main() is the staring point for all apps followed by runApp(....)
+void main() {
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          'I Am Rich',
+        ),
+      ),
+    )),
+  );
+}
