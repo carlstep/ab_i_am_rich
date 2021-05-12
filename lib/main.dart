27e784fc3ce1c10dcbc2b8ebccbae2b9200ba17d
carlstep <carlstep333@me.com>
2021-05-12 09:18:36 +0200

    created 'Image' widget + NetworkImage + Padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
 // main() is the starting point for all apps followed by runApp(....)
+// MaterialApp, Scaffold, AppBar, Colors and Text are Widgets that are Class Objects. The Class/Widget Objects contain properties.
 void main() {
   runApp(
     MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold( // scaffold creates the backbone structure for the child widgets.
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey,
-            title: Text(
-              'I Am Rich',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        //
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[800],
+          title: Text(
+            'I Am Rich',
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[700],
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image(
+              image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/material/app_bar.png'),
             ),
           ),
         ),
-        ),
+      ),
+    ),
   );
 }
